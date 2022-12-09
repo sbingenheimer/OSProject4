7284c64846f79a462ece8279d08562e8b463ef71
Samuel Bingenheimer <bingenheimer@vm-instunix-09.cs.wisc.edu>
2022-12-09 15:21:18 -0600

    added serverlib.c to handle commands on server side

---
+++ serverlib.c
@@ -0,0 +1,38 @@
+// GET CALLS FROM MFS AND DO OPERATIONS THEN RETURN
+
+// UDP read and write
+// get the command from the incoming message calls cooresponding method
+// in methods, we can update the input message to create a new outgoing message to return
+// gets return value from the method (-1, 0) and returns that value to client
+void getMSFCommand(){
+
+}
+
+
+
+
+
+int lookup (message_t message) {
+    return 0;
+}
+
+int stat (message_t message) {
+    return 0;
+}
+
+int write (message_t message) {
+    return 0;
+}
+
+int read (message_t message) {
+    return 0;
+}
+
+int unlink (message_t message) {
+    return 0;
+}
+
+int shutdown (message_t message) {
+    return 0;
+}
+
