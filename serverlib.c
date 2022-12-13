b60bb23b47037dc5ae441782102303af9fc9f8af
Samuel Bingenheimer <bingenheimer@royal-14.cs.wisc.edu>
2022-12-12 19:14:49 -0600

    changed around the set up for the server

--- serverlib.c
+++ serverlib.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
 #include "udp.h"
 
 #define BUFFER_SIZE (4096)
 int sd;
 message_t touse;
+sruct stat buff;
 
 // GET CALLS FROM MFS AND DO OPERATIONS THEN RETURN
 
@@ -13,6 +21,29 @@ message_t touse;
 // in methods, we can update the input message to create a new outgoing message to return
 // gets return value from the method (-1, 0) and returns that value to client
 void getMSFCommand(){
+
+    //Think we have to make the file image here first then create the socket and wait for messages
+
+    int fd = open(image, O_RDWR);
+    if (fd < 0) {
+        return -1;
+    }
+
+    //getting file information 
+    struct stat buff;
+    int rd = fstat(fd, &buff);
+    if (rd < 0) {
+        return -1;
+    }
+
+    int image_size = (int) sbuf.st_size;
+
+    void *image = mmap(NULL, image_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    assert(image != MAP_FAILED);
+
+
+   // so here we wait for messages
+
     sd = UDP_Open(10000); //not sure what to do here exactly. not sure what the port should be 
     assert(sd > -1);
     while (1) {
@@ -22,26 +53,43 @@ void getMSFCommand(){
 	int rc = UDP_Read(sd, &addr, message, BUFFER_SIZE);
 	printf("server:: read message [size:%d contents:(%s)]\n", rc, message);
 
+    int fd = open(image, O_RDWR);
+    if (fd < 0) {
+        return -1;
+    }
+
+    //getting file information 
+    struct stat buff;
+    int rd = fstat(fd, &buff);
+    if (rd < 0) {
+        return -1;
+    }
+
+    int image_size = (int) sbuf.st_size;
+
+    void *image = mmap(NULL, image_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    assert(image != MAP_FAILED);
+
     touse = (message_t) message;
     int resp;
 
     if (touse->mtype == MFS_Lookup) {
-        resp = lookup(touse);
+        resp = lookup(touse, image);
     }
     else if (touse->mtype == MFS_Stat) {
-        resp = stat(touse);
+        resp = stat(touse, image);
     }
     else if (touse->mtype == MFS_Write) {
-        resp = write(touse);
+        resp = write(touse, image);
     }
     else if (touse->mtype == MFS_Read) {
-        resp = read(touse);
+        resp = read(touse, image);
     }
     else if (touse->mtype == MFS_Unlink) {
-        resp = unlink(touse);
+        resp = unlink(touse, image);
     }
     else if (touse->mtype == MFS_Shutdown) {
-        resp = shutdown(touse);
+        resp = shutdown(touse, image);
     }
     else {
         touse->rc = -1;
@@ -51,7 +99,7 @@ void getMSFCommand(){
     rc = UDP_Write(sd, &addr, (char *) &touse, BUFFER_SIZE);
 	printf("server:: reply\n");
 
-    
+
     }
     return 0; 
 
@@ -59,29 +107,29 @@ void getMSFCommand(){
 
 
 
+int lookup (message_t message, void* image) {
 
-
-int lookup (message_t message) {
+    
     return 0;
 }
 
-int stat (message_t message) {
+int stat (message_t message, void* image) {
     return 0;
 }
 
-int write (message_t message) {
+int write (message_t message, void* image) {
     return 0;
 }
 
-int read (message_t message) {
+int read (message_t message, void* image) {
     return 0;
 }
 
-int unlink (message_t message) {
+int unlink (message_t message, void* image) {
     return 0;
 }
 
-int shutdown (message_t message) {
+int shutdown (message_t message, void* image) {
     return 0;
 }
 
