60aec7987fa60dd80cbecb03110aea4a43268eb5
sbingenheimer <79818049+sbingenheimer@users.noreply.github.com>
2022-12-16 21:03:38 -0600

    Update client.c

--- client.c
+++ client.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "udp.h"
+#include "mfs.h"
+#include "message.h"
 
 #define BUFFER_SIZE (1000)
 
@@ -10,18 +12,19 @@ int main(int argc, char *argv[]) {
     int sd = UDP_Open(20000);
     int rc = UDP_FillSockAddr(&addrSnd, "localhost", 10000);
 
-    char message[BUFFER_SIZE];
-    sprintf(message, "hello world");
+    message_t message;
+    message.mtype = "MFS_Lookup";
+    message.lookupName = "hi";
 
-    printf("client:: send message [%s]\n", message);
-    rc = UDP_Write(sd, &addrSnd, message, BUFFER_SIZE);
+    printf("client:: send message [%s]\n", message.mtype);
+    rc = UDP_Write(sd, &addrSnd, (char*)&message, BUFFER_SIZE);
     if (rc < 0) {
 	printf("client:: failed to send\n");
 	exit(1);
     }
 
     printf("client:: wait for reply...\n");
-    rc = UDP_Read(sd, &addrRcv, message, BUFFER_SIZE);
-    printf("client:: got reply [size:%d contents:(%s)\n", rc, message);
+    rc = UDP_Read(sd, &addrRcv, (char*)&message, BUFFER_SIZE);
+    printf("client:: got reply [size:%d contents:(%s)\n", rc, message.response);
     return 0;
 }
